a5df4b736eb058a994ac254d0ea9eaf1ae61474b
gsvn32 <gvn119015032@gmail.com>
2023-02-24 17:56:56 -0600

    Added subprocess in master for workers

--- master.py
+++ master.py
@@ -5,12 +5,49 @@
 import xmlrpc.client
 from xmlrpc.server import SimpleXMLRPCServer
 import sys
+import subprocess
+
 # Restrict to a particular path.
 workers = {
     'worker-am': "23001",
     'worker-nz': "23002"
 }
 
+import psutil
+def is_not_alive(group):
+    # Replace "process_name" with the name of the process you want to check
+    
+    # Get a list of all running processes
+    for process in psutil.process_iter():
+        try:
+            # Get the process name and check if it matches the specified name
+            #process.cmdline() returns ['python3', 'worker.py', '23001', 'am']            
+            temp= process.cmdline()
+            #print(temp)
+            if len(temp) >3 and temp[0]=='python3':
+                if temp[1]=='worker.py' and temp[3] == group:
+                    return False
+            #print("Process status:", process.status())
+            #break
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            # Handle any exceptions that might occur while accessing the process information
+            pass
+    #else:
+    #Process is not running
+    return True
+
+def create_process(port,group):
+    # Replace "command" with the command you want to run
+    command = "python3 worker.py "+str(port)+" "+group
+    print("creating new process with '"+command+"'")
+
+    # Start the process
+    process = subprocess.Popen(command, shell=True)
+def check_proc_status():
+    if is_not_alive('am'):
+        create_process(23001,"am")
+    if is_not_alive('nz'):
+        create_process(23002,"nz")
 
 def getbylocation(location):
     # TODO
@@ -23,7 +60,7 @@ def getbylocation(location):
     
     #Results from worker-nz
     worker= 'worker-nz'
-    proxy2= xmlrpc.client.ServerProxy(f"http://localhost:{workers[worker]}/")    
+    proxy2= xmlrpc.client.ServerProxy(f"http://localhost:{workers[worker]}/") 
     result2 = proxy2.getbylocation(location)
     if result1["error"] and result2["error"]:
         return "No data found with given inputs"
@@ -83,6 +120,7 @@ def getbyyear(location, year):
 def main():
     port = int(sys.argv[1])
     server = SimpleXMLRPCServer(("localhost", port))
+    check_proc_status()
     print(f"Listening on port {port}...")
 
     # TODO: register RPC functions
