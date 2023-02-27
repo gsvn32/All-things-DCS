d7e403e64f341718048ba682e7e877aaa18f6d3a
Jagannath Guptha Kasula <jagan@jagannaths-air.mynetworksettings.com>
2023-02-27 14:00:20 -0600

    Add comments in master and worker files

--- master.py
+++ master.py
@@ -1,7 +1,3 @@
-#Group 10 - PA1 - 2023/02/22
-#Master server
-#
-#
 import xmlrpc.client
 from xmlrpc.server import SimpleXMLRPCServer
 import sys
@@ -9,126 +5,163 @@ import subprocess
 import psutil
 import threading
 import queue
-import time
+
 # Restrict to a particular path.
 active_workers = {
     'worker-am': "23001",
     'worker-nz': "23002"
 }
-workers= {
+
+workers = {
     'worker-am': [23001],
     'worker-nz': [23002]
 }
-req_count=0
-# create a queue to hold tasks
-#task_queue = queue.Queue()
-# Request queue
+
+req_count = 0
+
+# Request queue for queuing the RPC requests coming from the Client.
 request_queue = queue.Queue(maxsize=20)
+
 # create a pool of worker threads
 threads = {}
 
+
 def find_free_port():
+    # Find a free port on the machine by creating a new socket and binding it
     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
         s.bind(('', 0))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         return s.getsockname()[1]
+
+
 def is_not_alive(group):
-    # Get a list of all running processes
+    # Check if a process with the specified group is already running
     for process in psutil.process_iter():
         try:
-            # Get the process cmdline and check if it matches the specified values
-            #process.cmdline() returns ['python3', 'worker.py', '23001', 'am']            
-            temp= process.cmdline()
-            #print(temp)
-            if len(temp) >3 and temp[0]=='python3':
-                if temp[1]=='worker.py' and temp[3] == group:
+            # Get the command line arguments of the process
+            temp = process.cmdline()
+            if len(temp) > 3 and temp[0] == 'python3':
+                if temp[1] == 'worker.py' and temp[3] == group:
                     return False
-            #print("Process status:", process.status())
-            #break
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             # Handle any exceptions that might occur while accessing the process information
             print("Either psutil AccessDenied or psutil ZombieProcess")
-    #else:
-    #Process is not running
-    return True
-def create_process(port,group):
-    # Replace "command" with the command you want to run
-    command = "python3 worker.py "+str(port)+" "+group
-    print("creating new process with '"+command+"'")
 
-    # Start the process
-    process = subprocess.Popen(command, shell=True)   
+    # If no process with the specified group is running, return True
+    return True
+
+
+def create_process(port, group):
+    # Create a command to start a new worker process
+    command = "python3 worker.py " + str(port) + " " + group
+    print("creating new process with '" + command + "'")
+
+    # Start a new process to run the worker script
+    subprocess.Popen(command, shell=True)
+
+
 def is_qfull(port):
-    #print(f"http://localhost:{port}/")
-    temps=xmlrpc.client.ServerProxy(f"http://localhost:{port}/")
-    print("is queue full at "+str(port)+" "+str(temps.is_queue_full()))
+    # Check if the worker on the specified port has a full queue
+    temps = xmlrpc.client.ServerProxy(f"http://localhost:{port}/")
+    print("is queue full at " + str(port) + " " + str(temps.is_queue_full()))
     return temps.is_queue_full()
+
+
 def check_proc_status():
+    # Check if worker-am is alive, if not create a new process and update active_workers dictionary
     if is_not_alive('am'):
-        create_process(23001,"am")
+        create_process(23001, "am")
         print("sending work to worker-am at 23001")
-        active_workers['worker-am']="23001"
+        active_workers['worker-am'] = "23001"
+
+    # Check if worker-nz is alive, if not create a new process and update active_workers dictionary
     if is_not_alive('nz'):
-        create_process(23002,"nz")
+        create_process(23002, "nz")
         print("sending work to worker-nz at 23002")
-        active_workers['worker-nz']="23002"
-    
-    for j in ['worker-am','worker-nz']:
-        all_q_full=False
+        active_workers['worker-nz'] = "23002"
+
+    # Check if queues of all workers are full
+    for j in ['worker-am', 'worker-nz']:
+        all_q_full = False
         for i in workers[j]:
-            #print(workers)
-            #print(i)
+            # If queue of a worker is full, mark all_q_full flag as true and break the inner loop
             if is_qfull(i):
-                all_q_full=True
+                all_q_full = True
             else:
-                all_q_full=False
-                active_workers[j]=i
+                # If queue of a worker is not full, mark all_q_full flag as false and update active_workers dictionary
+                all_q_full = False
+                active_workers[j] = i
+
+        # If queues of all workers are full, create a new worker process and update workers and active_workers dictionary
         if all_q_full:
-            temp_port=find_free_port()
-            create_process(temp_port,j.split("-")[1])
-            print("sending work to "+j+" at "+str(temp_port))
+            temp_port = find_free_port()
+            create_process(temp_port, j.split("-")[1])
+            print("sending work to " + j + " at " + str(temp_port))
             workers[j].append(str(temp_port))
-            active_workers[j]=str(temp_port)
-    print("active workers am: "+str(active_workers['worker-am'])+" nz: "+str(active_workers['worker-nz']))
-    
-    
+            active_workers[j] = str(temp_port)
+
+    # Print the status of active workers
+    print("active workers am: " + str(active_workers['worker-am']) + " nz: " + str(active_workers['worker-nz']))
+
+
 class Query:
-    def __init__(self,qtype, queries):
+    def __init__(self, qtype, queries):
         self.name = queries[0]
         self.location = queries[0]
         self.year = queries[1]
         self.qtype = qtype
+
     def say_hello(self):
+        # This method prints a greeting message, including the name and location of the Query object.
         print(f"Hello, my name is {self.name} and I am {self.location}")
-        
+
     def process_req(self):
-        #check if workers are alive
+        # This method processes the query request.
+        # It first checks if the workers are alive, and then performs the query according to the query type.
+
+        # check if workers are alive
         check_proc_status()
-        if self.qtype==1:            
+        if self.qtype == 1:
+            # This block of code handles the case when the query is for a person with a given name.
+            # It determines which worker to use based on the first letter of the name, and sends the request to that worker.
+            # It then prints some information about the request and the returned result, and returns the result.
+
             # bmc - print statement to see receipt of request
             print(f'Client => Asking for person with {self.name}')
-            if self.name[0].lower() >='a' and self.name[0].lower()<='m':
-                self.worker= 'worker-am'        
+
+            # Determine which worker to use based on the first letter of the name.
+            if self.name[0].lower() >= 'a' and self.name[0].lower() <= 'm':
+                self.worker = 'worker-am'
             else:
-                self.worker= 'worker-nz'
-            self.proxy= xmlrpc.client.ServerProxy(f"http://localhost:{active_workers[self.worker]}/")
+                self.worker = 'worker-nz'
+
+            # Send the request to the worker using XML-RPC.
+            self.proxy = xmlrpc.client.ServerProxy(f"http://localhost:{active_workers[self.worker]}/")
             print(f"http://localhost:{active_workers[self.worker]}/")
-            self.result = self.proxy.handle_request('getbyname',self.name)
-            print(f'The returned result = {self.result}')    
+
+            self.result = self.proxy.handle_request('getbyname', self.name)
+            print(f'The returned result = {self.result}')
             return self.result["result"]
-            
-        elif self.qtype==2:
+
+        elif self.qtype == 2:
+            # This block of code handles the case when the query is for people who lived in a given location.
+            # It sends the request to both workers and combines the results.
+            # If one of the workers returns an error, it returns the result from the other worker.
+
             # bmc - print statement to see receipt of request
             print(f'Client => Asking for person lived at {self.location}')
-            #Results from worker-am
-            self.worker= 'worker-am'
-            self.proxy1= xmlrpc.client.ServerProxy(f"http://localhost:{active_workers[self.worker]}/")
-            self.result1 = self.proxy1.handle_request('getbylocation',self.location)   
-            #Results from worker-nz
-            self.worker= 'worker-nz'
-            self.proxy2= xmlrpc.client.ServerProxy(f"http://localhost:{active_workers[self.worker]}/") 
-            self.result2 = self.proxy2.handle_request('getbylocation',self.location)    
-            #Error handling
+
+            # Results from worker-am
+            self.worker = 'worker-am'
+            self.proxy1 = xmlrpc.client.ServerProxy(f"http://localhost:{active_workers[self.worker]}/")
+            self.result1 = self.proxy1.handle_request('getbylocation', self.location)
+
+            # Results from worker-nz
+            self.worker = 'worker-nz'
+            self.proxy2 = xmlrpc.client.ServerProxy(f"http://localhost:{active_workers[self.worker]}/")
+            self.result2 = self.proxy2.handle_request('getbylocation', self.location)
+
+            # Error handling
             if self.result1["error"] and self.result2["error"]:
                 return "No data found with given inputs"
             elif self.result1["error"]:
@@ -136,21 +169,27 @@ class Query:
             elif self.result2["error"]:
                 return self.result1["result"]
             else:
-                return self.result1["result"]+self.result2["result"]
-        elif self.qtype==3:
+                return self.result1["result"] + self.result2["result"]
+
+        elif self.qtype == 3:
+            # This block of code handles the case when the query is for people who lived in a given location and year.
+            # It sends the request to both workers and combines the results.
+            # If one of the workers returns an error, it returns the result from the other worker.
+
             # bmc - print statement to see receipt of request
             print(f'Client => Asking for person lived in {self.location} in {self.year}')
-            #Results from worker-am
-            self.worker= 'worker-am'
-            self.proxy1= xmlrpc.client.ServerProxy(f"http://localhost:{active_workers[self.worker]}/")
-            self.result1 = self.proxy1.handle_request('getbyyear',[self.location, self.year])
-            
-            #Results from worker-nz
-            self.worker= 'worker-nz'
-            self.proxy2= xmlrpc.client.ServerProxy(f"http://localhost:{active_workers[self.worker]}/")    
-            self.result2 = self.proxy2.handle_request('getbyyear',[self.location, self.year])
-            
-            #Error handling
+
+            # Results from worker-am
+            self.worker = 'worker-am'
+            self.proxy1 = xmlrpc.client.ServerProxy(f"http://localhost:{active_workers[self.worker]}/")
+            self.result1 = self.proxy1.handle_request('getbyyear', [self.location, self.year])
+
+            # Results from worker-nz
+            self.worker = 'worker-nz'
+            self.proxy2 = xmlrpc.client.ServerProxy(f"http://localhost:{active_workers[self.worker]}/")
+            self.result2 = self.proxy2.handle_request('getbyyear', [self.location, self.year])
+
+            # Error handling
             if self.result1["error"] and self.result2["error"]:
                 return "No data found with given inputs"
             elif self.result1["error"]:
@@ -158,32 +197,45 @@ class Query:
             elif self.result2["error"]:
                 return self.result1["result"]
             else:
-                return self.result1["result"]+self.result2["result"]   
-                
-        
+                return self.result1["result"] + self.result2["result"]
+
+
+# Request is processed here by mapping to the respective method.
 def process_request(request):
-    # Process the request here
+    # Extract the method and args from the request dictionary
     method = request['method']
     args = request['args']
-    print(args)
-    if method == 'getbyname':
-        return getbyname(args)
-    elif method == 'getbylocation':
-        return getbylocation(args)
-    elif method == 'getbyyear':
-        return getbyyear(args[0],args[1])
+
+    # Define a switch statement that maps the method to the corresponding function
+    switcher = {
+        'getbyname': getbyname(args),
+        'getbylocation': getbylocation(args),
+        'getbyyear': getbyyear(args[0], args[1]),
+    }
+
+    # Call the function corresponding to the method and return the result
+    return switcher.get(method, "Invalid method")
+
 
 def request_worker():
+    # This function processes requests from the request queue and sends the result back to the client
     while True:
+        # Get the next request from the queue
         request = request_queue.get()
         print(request)
+        # Process the request
         result = process_request(request)
         print(result)
+        # Put the result in the response queue
         request['response'].put(result)
+        # Mark the request as done
         request_queue.task_done()
 
+
 def handle_request_server(method, args):
-    # Create a new queue to hold the response
+    # This function creates a new request dictionary and puts it in the request queue
+    # It also creates a new response queue to hold the response from the request worker
+    # It waits for the response from the request worker and returns the response to the client
     response_queue = queue.Queue()
 
     # Create a new request dictionary
@@ -197,51 +249,63 @@ def handle_request_server(method, args):
         request_queue.put(request, block=False)
     except queue.Full:
         raise Exception('Request queue is full')
+
     print("Processing request from client")
+
     print(response_queue.empty())
+
     # Wait for the response
     while True:
-    	if not response_queue.empty():
+        if not response_queue.empty():
+            # Get the response from the response queue
             response = response_queue.get()
-            print("in here")
-            print("out here")
             print(response)
+            # Mark the response as done
             response_queue.task_done()
+            # Return the response to the client
             return response
 
+
 def getbyname(name):
-    query1=Query(1,[name,2000])
+    # This function creates a new Query object with 'name' and calls the process_req() method to handle the request
+    query1 = Query(1, [name, 2000])
     query1.say_hello()
     return query1.process_req()
 
-    
+
 def getbylocation(location):
-    query1=Query(2,[location,2000])
+    # This function creates a new Query object with 'location' and calls the process_req() method to handle the request
+    query1 = Query(2, [location, 2000])
     query1.say_hello()
     return query1.process_req()
+
 
 def getbyyear(location, year):
-    query1=Query(3,[location,year])
+    # This function creates a new Query object with 'location' and 'year' and calls the process_req() method to handle the request
+    query1 = Query(3, [location, year])
     query1.say_hello()
     return query1.process_req()
-
 
 
 def main():
+    # This function starts the request worker thread, creates a new SimpleXMLRPCServer object and registers the handle_request_server function
     port = int(sys.argv[1])
+
+    # Create a SimpleXMLRPCServer object on the specified port
     server = SimpleXMLRPCServer(("localhost", port))
     check_proc_status()
     print(f"Listening on port {port}...")
-    # Start the request worker thread
+
+    # Start the server thread
     t = threading.Thread(target=request_worker)
     t.daemon = True
     t.start()
 
-    # TODO: register RPC functions
-    # ben curtis - added register functions
+    # Register function to be used by the XML-RPC server
     server.register_function(handle_request_server, 'handle_request')
     server.serve_forever()
 
 
 if __name__ == '__main__':
+    # Entry point of the script
     main()
--- worker.py
+++ worker.py
@@ -3,14 +3,15 @@ import queue
 import threading
 from xmlrpc.server import SimpleXMLRPCServer
 import sys
-import time
-import asyncio
-# Storage of data
+
+# Data table for loading the data.
 data_table = {}
 
-# Request queue
+# Request queue for queuing the RPC requests coming from the Master.
 request_queue = queue.Queue(maxsize=20)
 
+
+# Method for loading the data into the table based on the group ('am' or 'nz')
 def load_data(group):
     global data_table
     with open('./data/data-' + group + '.json', 'r') as f:
@@ -18,39 +19,55 @@ def load_data(group):
         data_table = json.loads(json_data)
     return data_table
 
+
+# Request is processed here by mapping to the respective method.
 def process_request(request):
-    # Process the request here
+    # Extract the method and args from the request dictionary
     method = request['method']
     args = request['args']
-    print(args)
-    if method == 'getbyname':
-        return getbyname(args)
-    elif method == 'getbylocation':
-        return getbylocation(args)
-    elif method == 'getbyyear':
-        return getbyyear(args[0],args[1])
 
+    # Define a switch statement that maps the method to the corresponding function
+    switcher = {
+        'getbyname': getbyname(args),
+        'getbylocation': getbylocation(args),
+        'getbyyear': getbyyear(args[0], args[1]),
+    }
+
+    # Call the function corresponding to the method and return the result
+    return switcher.get(method, "Invalid method")
+
+
+# Thread function for processing requests in the queue
 def request_worker():
     while True:
+        # Get the next request from the queue
         request = request_queue.get()
         print(request)
-        result =  process_request(request)
+
+        # Process the request and get the result
+        result = process_request(request)
         print(result)
+
+        # Put the result in the response queue for the request
         request['response'].put(result)
+
+        # Mark the request as done
         request_queue.task_done()
 
+
+# Function for handling requests from the Master
 def handle_request(method, args):
     # Create a new queue to hold the response
     response_queue = queue.Queue()
 
-    # Create a new request dictionary
+    # Create a new request dictionary with method, args, and response queue
     request = {
         'method': method,
         'args': args,
         'response': response_queue
     }
 
-    # Put the request in the queue
+    # Try to put the request in the queue, raise an exception if the queue is full
     try:
         request_queue.put(request, block=False)
     except queue.Full:
@@ -58,23 +75,32 @@ def handle_request(method, args):
 
     # Wait for the response
     while True:
-    	if not response_queue.empty():
+        if not response_queue.empty():
+            # Get the response from the queue
             response = response_queue.get()
-            print("in here")
 
-            print("out here")
+            # Print the response
             print(response)
+
+            # Mark the response as done
             response_queue.task_done()
 
+            # Return the response to the Master
             return response
-    	
 
+
+# Function for checking if the request queue is full
 def is_queue_full():
     return request_queue.full()
 
+
+# Function for getting data by name
 def getbyname(name):
     try:
+        # Find the record with the given name in the data table
         record = data_table[name.lower()]
+
+        # Return the record as a dictionary.
         return {
             'error': False,
             'result': record
@@ -82,14 +108,16 @@ def getbyname(name):
     except KeyError:
         # Handle the case where the name is not found
         print(f"No record found for name '{name}'")
+
+        # Return an error message as a dictionary with error=True and result='No data found with given name'
         return {
             'error': True,
             'result': 'No data found with given name'
         }
 
 
+# Function for getting data by location
 def getbylocation(location):
-
     # Create an empty list to store the matching records
     matching_records = []
 
@@ -97,12 +125,14 @@ def getbylocation(location):
     for record in data_table.values():
         if record['location'].casefold() == location.casefold():
             matching_records.append(record)
-    
-    if matching_records == []:
+
+    # Return error as true when no records are found
+    if not matching_records:
         return {
-        'error': True,
-        'result': 'No matching recoreds'
+            'error': True,
+            'result': 'No matching records'
         }
+
     # Return the matching records
     return {
         'error': False,
@@ -111,7 +141,6 @@ def getbylocation(location):
 
 
 def getbyyear(location, year):
-
     # Create an empty list to store the matching records
     matching_records = []
 
@@ -119,11 +148,14 @@ def getbyyear(location, year):
     for record in data_table.values():
         if record['location'].casefold() == location.casefold() and record['year'] == year:
             matching_records.append(record)
-    if matching_records == []:
+
+    # Return error as true when no records are found
+    if not matching_records:
         return {
-        'error': True,
-        'result': 'No matching recoreds'
+            'error': True,
+            'result': 'No matching recoreds'
         }
+
     # Return the matching records
     return {
         'error': False,
@@ -132,27 +164,35 @@ def getbyyear(location, year):
 
 
 def main():
+    # Check if the command line arguments are correct
     if len(sys.argv) < 3:
         print('Usage: worker.py <port> <group: am or nz>')
         sys.exit(0)
 
+    # Extract the command line arguments
     port = int(sys.argv[1])
     group = sys.argv[2]
 
-    # Load the data
+    # # Load the data based on the provided group
     load_data(group)
-    
+
+    # Create a SimpleXMLRPCServer object on the specified port
     server = SimpleXMLRPCServer(("localhost", port))
     print(f"Listening on port {port}...")
-    
+
     # Start the request worker thread
     t = threading.Thread(target=request_worker)
     t.daemon = True
     t.start()
+
+    # Register two functions to be used by the XML-RPC server
     server.register_function(is_queue_full, 'is_queue_full')
     server.register_function(handle_request, 'handle_request')
+
+    # Start serving requests
     server.serve_forever()
 
 
 if __name__ == '__main__':
+    # Entry point of the script
     main()
